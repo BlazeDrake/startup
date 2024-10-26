0f1eb3169d11432c76809b3ffa3105c3ab88658b
BlazeDrake <blaze.drake@outlook.com>
2024-10-25 19:00:22 -0600

    Ported html pages to jsx
    Everything uses routing now

--- src/app.jsx
+++ src/app.jsx
@@ -6,16 +6,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return (<div className='body bg-dark text-light'>
-    <header className="container-fluid">
-      <h1>Profile Tracker</h1>
-    </header>
-    <main className="bg-secondary text-dark">App content goes here</main>
-    <footer className="bg-dark text-white-90 container-fluid">
-      <div className="container-fluid">
-        <span className="text-reset">Damon Stevens</span>
-        <a href="https://github.com/BlazeDrake/startup">GitHub</a>
+  return (
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className="container-fluid">
+          <h1>Profile Tracker</h1>
+        </header>
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <footer className="bg-dark text-white-90 container-fluid">
+          <div className="container-fluid">
+            <span className="text-reset">Damon Stevens</span>
+            <a href="https://github.com/BlazeDrake/startup">GitHub</a>
+          </div>
+        </footer>
       </div>
-    </footer>
-  </div>);
+    </BrowserRouter>
+
+  );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import './login.css';
 
 export function Login() {
   return (
     <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+      <div className="container-fluid bg-secondary text-dark">
+        <h1>Login to get started</h1>
+        <form method="get" action="profile">
+              <div className="input-group mb-3 login-form">
+                <div className="login-item">
+                  <span className="input-group-text">@</span>
+                  <input className="form-control" type="text" placeholder="your@email.com" />
+                </div>
+                <div className="login-item">
+                  <span className="input-group-text">🔒</span>
+                  <input className="form-control" type="password" placeholder="password" />
+                </div>
+              </div>
+              <button type="submit" className="btn btn-primary">Login</button>
+              <button type="submit" className="btn btn-success">Create</button>
+        </form>
+      </div>
+        <div className="about-info">
+          <h2>About</h2>
+          Have you ever had a hard time keeping track of all of your different profiles, with details such as username and which profile picture you're using?
+          Well, Profile Tracker is the thing for you! You can create different username groups with it, and then track which services you use that name for!
+          You can also store your profile picture there, to make it easy to keep track of what you're currently using and if you want to change it. You can even generate one to download!
+          Create a profile or log in to get started!
+        </div>
     </main>
   );
 }
