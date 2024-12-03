8a593e6c6b578efe04920fc069db5fc5c19bd4c4
BlazeDrake <blaze.drake@outlook.com>
2024-12-03 08:43:04 -0700

    Started websocket connection frontend
    Added code from simon to the frontend. Also made it so temporarily, browsers receive their own message for testing purposes

--- src/profile/updateNotifier.js
+++ src/profile/updateNotifier.js
@@ -17,6 +17,24 @@ const UpdateEvent = {
     handlers = [];
   
     constructor() {
+
+      //Set up the websocket
+      let port = window.location.port;
+      const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+      this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+      this.socket.onopen = (event) => {
+        this.receiveEvent(new EventMessage('System', UpdateEvent.System, { msg: 'connected' }));
+      };
+      this.socket.onclose = (event) => {
+        this.receiveEvent(new EventMessage('System', UpdateEvent.System, { msg: 'disconnected' }));
+      };
+      this.socket.onmessage = async (msg) => {
+        try {
+          const event = JSON.parse(await msg.data.text());
+          this.receiveEvent(event);
+        } catch {}
+      };
+
       // Simulate chat messages that will eventually come over WebSocket
       setInterval(() => {
         const id = Math.floor(Math.random() * 3000);
@@ -28,7 +46,7 @@ const UpdateEvent = {
   
     broadcastEvent(from, type, value) {
       const event = new EventMessage(from, type, value);
-      this.receiveEvent(event);
+      //this.receiveEvent(event);
     }
   
     addHandler(handler) {
