a2ec6b23e6ffd6726f6d6bb4d09e66d2abb74eb2
BlazeDrake <blaze.drake@outlook.com>
2024-12-03 08:51:24 -0700

    Manually set the port to 3000
    This is for testing purposes, so websocket actually connects from npm run dev

--- src/profile/updateNotifier.js
+++ src/profile/updateNotifier.js
@@ -19,7 +19,7 @@ const UpdateEvent = {
     constructor() {
 
       //Set up the websocket
-      let port = window.location.port;
+      let port =3000;// window.location.port;
       const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
       this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
       this.socket.onopen = (event) => {
